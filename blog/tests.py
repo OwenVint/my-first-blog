2f76643b32c565dcb7789096d7937d4f56b6f333
Owen <owenvint@hotmail.com>
2020-08-25 20:00:44 +0100

    Added Basic Testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from blog.views import post_list  
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, post_list)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = post_list(request)  
+        html = response.content.decode('utf8')  
+        self.assertIn('<title>The Blog of Owen Vint</title>', html)  
+        self.assertTrue(html.endswith('</html>'))  
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*_enil@hx)plw0d59$14ku8(gug9d%_0)^ge+yr=61#^ii8anz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
