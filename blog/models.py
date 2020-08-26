6462806398d11b4a18a1b78d8ac59c27a3dddb0d
Owen <owenvint@hotmail.com>
2020-08-26 20:24:22 +0100

    Added CV Form so data can be added

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    overview = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, ProfileForm
+from .models import Post, Profile
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -40,4 +40,19 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def profile(request):
-    return render(request, 'blog/profile.html')
+    profile = get_object_or_404(Profile)
+    return render(request, 'blog/profile.html',{'profile': profile})
+
+def profile_edit(request, pk):
+    profile = get_object_or_404(Profile, pk = 1)
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=post)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('profile', pk = 1)
+    else:
+        form = ProfileForm(instance = profile)
+    return render(request, 'blog/profile/profile_edit.html',{'profile' : profile})
